49ea486ddade82b93c496683a1f3e013cdd34af0
varshith078 <varshith0078@gmail.com>
2025-06-27 00:45:37 +0530

    Remove unwanted root files from remote, keep only terraform-23 folder

--- output.tf
+++
@@ -1,3 +0,0 @@
-output "availability_zones" {
-  value = data.aws_availability_zones.available.*.names
-}
--- provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region                   = var.region_name
-  shared_credentials_files = ["C:/Users/Admin/.aws/credentials"]
-}
